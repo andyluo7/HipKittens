07c46c8d4aea415c8b7ea34842bfd02437e1a195
Simran Arora <emailsimran@gmail.com>
2025-08-28 14:37:19 -0500

    micro

--- kernels/TK/attn/gqa_backwards/micros/vector_ops/test_python.py
+++ kernels/TK/attn/gqa_backwards/micros/vector_ops/test_python.py
@@ -15,31 +15,47 @@ torch.manual_seed(0)
 
 b = 1
 h = 1
-n = 512
+n = 32
 d = 32
 
 # pytorch
-x = torch.randn((b, h, n, d), dtype=torch.float32, device='cuda')
-vec = torch.randn((b, h, 1, d), dtype=torch.float32, device='cuda')
-# y = x - vec
-y = x.sum(dim=-1, keepdim=True)
+x_a = torch.randn((b, h, n, d), dtype=torch.bfloat16, device='cuda')
+x_b = torch.randn((b, h, n, d), dtype=torch.bfloat16, device='cuda')
+x_accum = torch.zeros((b, h, n, d), dtype=torch.float32, device='cuda')
+# y = x.sum(dim=-1, keepdim=True)
+y = torch.matmul(x_a, x_b.transpose(-2, -1)).float()
+# y = x_b
 
 # tk
-y_tk = torch.zeros_like(y)
-tk_kernel.dispatch_micro(x, vec, y_tk)
+y_tk = torch.zeros_like(y).float()
+tk_kernel.dispatch_micro(x_a, x_b, y_tk, x_accum)
+
+print(y.shape, y_tk.shape)
 
 # check
 diff = (y - y_tk).abs()
 max_diff = diff.max()
-print(y.shape, vec.shape, x.shape)
+print(y.shape,  x_a.shape, x_b.shape)
+
+
+# print(y[0, 0, 0:2, :8])
+print(diff[0, 0, 0:, :])
+num_diff = (diff > 1e-3).sum()
+print(f"num_diff: {num_diff} / {diff.numel()}")
+# print()
+
+# print(x_b[0, 0, 0:, :].sum(dim=-1))
+# print(y_tk[0, 0, 0:1, :])
+# print(y[0, 0, 0:1, :])
+
+# print(x_b[0, 0, 0:16, :])
+# print(y_tk[0, 0, 0:16, :])
+# print(y[0, 0, 0:16, :])
+
+# print(y[0, 0, 8:10, :8])
+# print(y_tk[0, 0, 8:10, :8])
 print(f"diff: {max_diff}")
 
 
-print(y[0, 0, 0:2, :8])
-print(y_tk[0, 0, 0:2, :8])
-print()
-
-print(y[0, 0, 8:10, :8])
-print(y_tk[0, 0, 8:10, :8])
 
 
--- kernels/TK/attn/gqa_backwards/micros/vector_ops/vector_micro.cpp
+++ kernels/TK/attn/gqa_backwards/micros/vector_ops/vector_micro.cpp
@@ -4,16 +4,15 @@ using namespace kittens;
 
 constexpr int b = 1;
 constexpr int h = 1;
-constexpr int n = 512;
+constexpr int n = 32;
 constexpr int d = 32;
 
 #define NUM_WARPS 1
 #define NUM_THREADS (kittens::WARP_THREADS * NUM_WARPS)
 
 struct micro_globals {
-    gl<float, -1, -1, -1, -1> in;
-    gl<float, -1, -1, -1, -1> in_vec;
-    gl<float, -1, -1, -1, -1> out;
+    gl<bf16, -1, -1, -1, -1> in_a, in_b; 
+    gl<float, -1, -1, -1, -1> out, in_accum;
     dim3 grid()  { return dim3(1); } 
     dim3 block() { return dim3(NUM_THREADS); } 
     size_t dynamic_shared_memory() { return MAX_SHARED_MEMORY-2048; }
@@ -22,23 +21,25 @@ struct micro_globals {
 __global__ __launch_bounds__(NUM_THREADS, 1)
 void micro_tk(const micro_globals g) {
 
-    rt_fl<n, d, row_l> tile_accum;
-    rt_fl<n, d, row_l>::col_vec vec;
-    load(tile_accum, g.in, {0, 0, 0, 0});
-    // load(vec, g.in_vec, {0, 0, 0, 0});
-    zero(vec);
-    __builtin_amdgcn_s_waitcnt(0);
-    __builtin_amdgcn_s_barrier();
-    __syncthreads();
-    // one(tile_accum);
-
-    // sub_col(tile_accum, tile_accum, vec);
-    row_sum(vec, tile_accum);
+    rt_bf<n, d, row_l> a_tile;
+    rt_bf<n, d, row_l> b_tile;
+    rt_fl<n, n, accum_col_l> tile_accum;
+    load(a_tile, g.in_a, {0, 0, 0, 0});
+    load(b_tile, g.in_b, {0, 0, 0, 0});
+    load(tile_accum, g.in_accum, {0, 0, 0, 0});
     __builtin_amdgcn_s_waitcnt(0);
     __builtin_amdgcn_s_barrier();
     __syncthreads();
 
-    store(g.out, vec, {0, 0, 0, 0});
+
+    mma_ABt(tile_accum, a_tile, b_tile, tile_accum);
+    __builtin_amdgcn_s_waitcnt(0);
+    __builtin_amdgcn_s_barrier();
+    __syncthreads();
+
+    
+    store(g.out, tile_accum, {0, 0, 0, 0});
+    
     __builtin_amdgcn_s_waitcnt(0);
     __builtin_amdgcn_s_barrier();
     __syncthreads();
@@ -53,6 +54,6 @@ void dispatch_micro(micro_globals g) {
 
 PYBIND11_MODULE(tk_kernel, m) {
     m.doc() = "tk_kernel python module";
-    py::bind_function<dispatch_micro>(m, "dispatch_micro", &micro_globals::in, &micro_globals::in_vec, &micro_globals::out);
+    py::bind_function<dispatch_micro>(m, "dispatch_micro", &micro_globals::in_a, &micro_globals::in_b, &micro_globals::out, &micro_globals::in_accum);
 }
 
